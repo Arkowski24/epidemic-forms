d636d95b9e772e99c4fbe1b6630e4948f90fcf1b
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-03-28 11:11:01 +0100

    Added basic datasource configuration

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,6 +24,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
