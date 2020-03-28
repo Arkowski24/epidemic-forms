12fd1748fc804d26a54fc406f20e77e105c58b74
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-03-28 10:33:33 +0100

    Added initialized backend

---
+++ backend/src/main/kotlin/pl/edu/agh/ki/covid19tablet/Covid19TabletApplication.kt
@@ -0,0 +1,11 @@
+package pl.edu.agh.ki.covid19tablet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Covid19TabletApplication
+
+fun main(args: Array<String>) {
+	runApplication<Covid19TabletApplication>(*args)
+}
---
+++ backend/src/test/kotlin/pl/edu/agh/ki/covid19tablet/Covid19TabletApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.edu.agh.ki.covid19tablet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Covid19TabletApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
