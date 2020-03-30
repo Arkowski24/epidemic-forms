0c9e748f0b30084f8425893c8797a830504bf89a
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-03-30 20:37:18 +0200

    Moved back to Kotlin

--- backend/src/main/java/pl/edu/agh/ki/covid19tablet/form_scheme/FormSchemeRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.edu.agh.ki.covid19tablet.form_scheme;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.edu.agh.ki.covid19tablet.form.Form;
-
-public interface FormSchemeRepository extends CrudRepository<Form, Integer> {}
---
+++ backend/src/test/kotlin/pl/edu/agh/ki/covid19tablet/Covid19TabletApplicationTests.kt
@@ -0,0 +1,15 @@
+package pl.edu.agh.ki.covid19tablet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootApplication
+@ActiveProfiles("test")
+class Covid19TabletApplicationTests {
+    @Test
+    fun contextLoad() {
+
+    }
+
+}
