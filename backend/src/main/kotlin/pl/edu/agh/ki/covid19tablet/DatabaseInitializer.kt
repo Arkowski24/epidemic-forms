23c85a8c3a783e22e9833aa13203c337569c5fe6
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-04-06 00:01:22 +0200

    Added tech demo

--- backend/src/main/kotlin/pl/edu/agh/ki/covid19tablet/DatabaseInitializer.kt
+++ backend/src/main/kotlin/pl/edu/agh/ki/covid19tablet/DatabaseInitializer.kt
@@ -11,6 +11,7 @@ import pl.edu.agh.ki.covid19tablet.schema.fields.DerivedField
 import pl.edu.agh.ki.covid19tablet.schema.fields.DerivedType
 import pl.edu.agh.ki.covid19tablet.schema.fields.FieldType
 import pl.edu.agh.ki.covid19tablet.schema.fields.SchemaFields
+import pl.edu.agh.ki.covid19tablet.schema.fields.SimpleField
 import pl.edu.agh.ki.covid19tablet.schema.fields.SliderField
 import pl.edu.agh.ki.covid19tablet.schema.fields.TextField
 import pl.edu.agh.ki.covid19tablet.schema.signature.SignatureField
@@ -163,6 +164,279 @@ class DatabaseInitializer {
                     )
                 )
             )
+            schemaRepository.save(
+                Schema(
+                    name = "Tech Demo",
+                    multiPage = false,
+                    fields = SchemaFields(
+                        simple = listOf(
+                            SimpleField(
+                                fieldNumber = 0,
+                                fieldType = FieldType.NORMAL,
+                                title = "Simple",
+                                description = "Simple",
+                                inline = false
+                            ),
+                            SimpleField(
+                                fieldNumber = 1,
+                                fieldType = FieldType.NORMAL,
+                                title = "Simple - NoDesc",
+                                inline = false
+                            ),
+                            SimpleField(
+                                fieldNumber = 2,
+                                fieldType = FieldType.NORMAL,
+                                title = "Simple - Inline",
+                                inline = true
+                            ),
+                            SimpleField(
+                                fieldNumber = 3,
+                                fieldType = FieldType.BLOCKED,
+                                title = "Simple - Blocked",
+                                description = "Simple",
+                                inline = false
+                            ),
+                            SimpleField(
+                                fieldNumber = 4,
+                                fieldType = FieldType.NORMAL,
+                                title = "Simple - NoDesc - Blocked",
+                                inline = false
+                            ),
+                            SimpleField(
+                                fieldNumber = 5,
+                                fieldType = FieldType.BLOCKED,
+                                title = "Simple - Inline - Blocked",
+                                inline = true
+                            )
+                        ),
+
+                        choice = listOf(
+                            ChoiceField(
+                                fieldNumber = 6,
+                                fieldType = FieldType.NORMAL,
+                                title = "Multi-choice",
+                                description = "Multi-choice",
+                                choices = listOf("A", "B", "C"),
+                                inline = false
+                            ),
+                            ChoiceField(
+                                fieldNumber = 7,
+                                fieldType = FieldType.NORMAL,
+                                title = "Multi-choice - Inline",
+                                choices = listOf("A", "B", "C"),
+                                inline = true
+                            ),
+                            ChoiceField(
+                                fieldNumber = 8,
+                                fieldType = FieldType.NORMAL,
+                                title = "Multi-choice",
+                                description = "Multi-choice - Blocked",
+                                choices = listOf("A", "B", "C"),
+                                inline = false
+                            ),
+                            ChoiceField(
+                                fieldNumber = 9,
+                                fieldType = FieldType.NORMAL,
+                                title = "Multi-choice - Inline - Blocked",
+                                choices = listOf("A", "B", "C"),
+                                inline = true
+                            ),
+
+                            ChoiceField(
+                                fieldNumber = 10,
+                                fieldType = FieldType.NORMAL,
+                                title = "Single-choice",
+                                description = "Single-choice",
+                                choices = listOf("A", "B", "C"),
+                                inline = false
+                            ),
+                            ChoiceField(
+                                fieldNumber = 11,
+                                fieldType = FieldType.NORMAL,
+                                title = "Single-choice - Inline",
+                                choices = listOf("A", "B", "C"),
+                                inline = true
+                            ),
+                            ChoiceField(
+                                fieldNumber = 12,
+                                fieldType = FieldType.NORMAL,
+                                title = "Single-choice - Inline - Two Values",
+                                choices = listOf("A", "B"),
+                                inline = true
+                            ),
+                            ChoiceField(
+                                fieldNumber = 13,
+                                fieldType = FieldType.BLOCKED,
+                                title = "Single-choice - Blocked",
+                                description = "Single-choice - Blocked",
+                                choices = listOf("A", "B", "C"),
+                                inline = false
+                            ),
+                            ChoiceField(
+                                fieldNumber = 14,
+                                fieldType = FieldType.BLOCKED,
+                                title = "Single-choice - Inline - Blocked",
+                                choices = listOf("A", "B", "C"),
+                                inline = true
+                            ),
+                            ChoiceField(
+                                fieldNumber = 15,
+                                fieldType = FieldType.BLOCKED,
+                                title = "Single-choice - Inline - Two Values - Blocked",
+                                choices = listOf("A", "B"),
+                                inline = true
+                            )
+                        ),
+                        derived = listOf(
+                            DerivedField(
+                                fieldNumber = 16,
+                                fieldType = FieldType.NORMAL,
+                                derivedType = DerivedType.BIRTHDAY_PESEL,
+                                titles = listOf("Derived 1", "Derived 2"),
+                                descriptions = listOf("Derived 1", "Derived 2"),
+                                inline = false
+                            ),
+                            DerivedField(
+                                fieldNumber = 17,
+                                fieldType = FieldType.NORMAL,
+                                derivedType = DerivedType.BIRTHDAY_PESEL,
+                                titles = listOf("Derived 1 - Inline", "Derived 2 - Inline"),
+                                inline = true
+                            ),
+                            DerivedField(
+                                fieldNumber = 18,
+                                fieldType = FieldType.BLOCKED,
+                                derivedType = DerivedType.BIRTHDAY_PESEL,
+                                titles = listOf("Derived 1 - Blocked", "Derived 2 - Blocked"),
+                                descriptions = listOf("Derived 1 - Blocked", "Derived 2 - Blocked"),
+                                inline = false
+                            ),
+                            DerivedField(
+                                fieldNumber = 19,
+                                fieldType = FieldType.BLOCKED,
+                                derivedType = DerivedType.BIRTHDAY_PESEL,
+                                titles = listOf("Derived 1 - Inline - Blocked", "Derived 2 - Inline - Blocked"),
+                                inline = true
+                            )
+                        ),
+
+                        slider = listOf(
+                            SliderField(
+                                fieldNumber = 20,
+                                fieldType = FieldType.NORMAL,
+                                inline = false,
+                                title = "Slider",
+                                minValue = 0.0,
+                                maxValue = 100.0,
+                                defaultValue = 50.0,
+                                step = 5.0
+                            ),
+                            SliderField(
+                                fieldNumber = 21,
+                                fieldType = FieldType.NORMAL,
+                                inline = true,
+                                title = "Slider - Inline",
+                                minValue = 0.0,
+                                maxValue = 100.0,
+                                defaultValue = 50.0,
+                                step = 5.0
+                            ),
+                            SliderField(
+                                fieldNumber = 22,
+                                fieldType = FieldType.BLOCKED,
+                                inline = false,
+                                title = "Slider - Blocked",
+                                minValue = 0.0,
+                                maxValue = 100.0,
+                                defaultValue = 50.0,
+                                step = 5.0
+                            ),
+                            SliderField(
+                                fieldNumber = 23,
+                                fieldType = FieldType.BLOCKED,
+                                inline = true,
+                                title = "Slider - Inline - Blocked",
+                                minValue = 0.0,
+                                maxValue = 100.0,
+                                defaultValue = 50.0,
+                                step = 5.0
+                            )
+                        ),
+                        text = listOf(
+                            TextField(
+                                fieldNumber = 24,
+                                fieldType = FieldType.NORMAL,
+                                inline = false,
+                                title = "Text",
+                                description = "Text",
+                                multiLine = false
+                            ),
+                            TextField(
+                                fieldNumber = 25,
+                                fieldType = FieldType.NORMAL,
+                                inline = false,
+                                title = "Text - Multiline",
+                                description = "Text - Multiline",
+                                multiLine = true
+                            ),
+                            TextField(
+                                fieldNumber = 26,
+                                fieldType = FieldType.NORMAL,
+                                inline = true,
+                                title = "Text - Inline",
+                                description = "Text",
+                                multiLine = false
+                            ),
+                            TextField(
+                                fieldNumber = 27,
+                                fieldType = FieldType.NORMAL,
+                                inline = true,
+                                title = "Text - Multiline - Inline",
+                                multiLine = true
+                            ),
+                            TextField(
+                                fieldNumber = 28,
+                                fieldType = FieldType.BLOCKED,
+                                inline = false,
+                                title = "Text",
+                                description = "Text - Blocked",
+                                multiLine = false
+                            ),
+                            TextField(
+                                fieldNumber = 29,
+                                fieldType = FieldType.BLOCKED,
+                                inline = false,
+                                title = "Text - Multiline - Blocked",
+                                description = "Text - Multiline - Blocked",
+                                multiLine = true
+                            ),
+                            TextField(
+                                fieldNumber = 30,
+                                fieldType = FieldType.BLOCKED,
+                                inline = true,
+                                title = "Text - Inline - Blocked",
+                                multiLine = false
+                            ),
+                            TextField(
+                                fieldNumber = 31,
+                                fieldType = FieldType.BLOCKED,
+                                inline = true,
+                                title = "Text - Multiline - Inline - Blocked",
+                                multiLine = true
+                            )
+                        )
+                    ),
+                    patientSignature = SignatureField(
+                        title = "Signature - Patient",
+                        description = "Signature - Patient"
+                    ),
+                    employeeSignature = SignatureField(
+                        title = "Signature - Employee",
+                        description = "Signature - Employee."
+                    )
+                )
+            )
+
             employeeRepository.save(
                 Employee(
                     username = "user",
