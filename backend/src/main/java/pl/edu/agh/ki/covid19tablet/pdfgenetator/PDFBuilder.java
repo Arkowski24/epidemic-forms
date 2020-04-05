932ed082deb01b4a5ee84520d52eee334d7161cc
Busiu <busiu69@gmail.com>
2020-04-06 01:36:18 +0200

    Another little fix.

--- backend/src/main/java/pl/edu/agh/ki/covid19tablet/pdfgenetator/PDFBuilder.java
+++ backend/src/main/java/pl/edu/agh/ki/covid19tablet/pdfgenetator/PDFBuilder.java
@@ -158,7 +158,7 @@ public class PDFBuilder {
             }
 
             if (currentField.getFieldType().equals(FieldType.SIMPLE)) {
-                continue;
+
             }
 
             if (currentField.getFieldType().equals(FieldType.SLIDER)) {
