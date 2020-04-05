9ce8dba7cd229bf2cb07a2180b2242e7a8478468
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-04-05 18:39:20 +0200

    Updated form schema

--- backend/src/main/kotlin/pl/edu/agh/ki/covid19tablet/DatabaseInitializer.kt
+++ backend/src/main/kotlin/pl/edu/agh/ki/covid19tablet/DatabaseInitializer.kt
@@ -11,7 +11,6 @@ import pl.edu.agh.ki.covid19tablet.schema.fields.DerivedField
 import pl.edu.agh.ki.covid19tablet.schema.fields.DerivedType
 import pl.edu.agh.ki.covid19tablet.schema.fields.FieldType
 import pl.edu.agh.ki.covid19tablet.schema.fields.SchemaFields
-import pl.edu.agh.ki.covid19tablet.schema.fields.SimpleField
 import pl.edu.agh.ki.covid19tablet.schema.fields.SliderField
 import pl.edu.agh.ki.covid19tablet.schema.fields.TextField
 import pl.edu.agh.ki.covid19tablet.schema.signature.SignatureField
@@ -33,88 +32,70 @@ class DatabaseInitializer {
                     name = "Formularz epidemiczny",
                     multiPage = false,
                     fields = SchemaFields(
-                        simple = listOf(
-                            SimpleField(
-                                fieldNumber = 0,
-                                title = "Dane osobowe",
-                                fieldType = FieldType.HIDDEN
-                            ),
-                            SimpleField(
-                                fieldNumber = 4,
-                                title = "Parametry życiowe",
-                                fieldType = FieldType.HIDDEN
-                            ),
-                            SimpleField(
-                                fieldNumber = 10,
-                                title = "Pytania epidemiczne",
-                                fieldType = FieldType.HIDDEN
-                            )
-                        ),
+                        simple = listOf(),
                         choice = listOf(
                             ChoiceField(
-                                fieldNumber = 8,
+                                fieldNumber = 7,
                                 fieldType = FieldType.HIDDEN,
-                                inline = false,
                                 title = "Częstość oddechu",
                                 choices = listOf("10-20", "20-30", "30-40", ">40")
                             ),
                             ChoiceField(
-                                fieldNumber = 11,
+                                fieldNumber = 8,
                                 title = "Czy miał Pan(i) kontakt z osoba zakażoną koronawirusem?",
                                 choices = listOf("TAK", "NIE")
                             ),
                             ChoiceField(
-                                fieldNumber = 12,
+                                fieldNumber = 9,
                                 title = "Czy miał Pan(i) kontakt z osobą w trakcie kwarantanny?",
                                 choices = listOf("TAK", "NIE")
                             ),
                             ChoiceField(
-                                fieldNumber = 13,
+                                fieldNumber = 10,
                                 title = "Czy w ciągu ostatnich dwóch tygodni przebywał Pan(i) za granicą?",
                                 choices = listOf("TAK", "NIE")
                             ),
                             ChoiceField(
-                                fieldNumber = 14,
+                                fieldNumber = 11,
                                 title = "Czy w ciągu ostatnich dwóch tygodni miał Pan(i) jakikolwiek kontakt z osobą, która powróciła z innego kraju?",
                                 choices = listOf("TAK", "NIE")
                             ),
                             ChoiceField(
-                                fieldNumber = 15,
+                                fieldNumber = 12,
                                 title = "Czy miał Pan(i) kontakt z osobą z objawami infekcji dróg oddechowych?",
                                 choices = listOf("TAK", "NIE")
                             ),
                             ChoiceField(
-                                fieldNumber = 16,
+                                fieldNumber = 13,
                                 title = "Czy pracuje Pan(i) w szpitalu lub przychodni lub zakładzie opiekuńczym?",
                                 choices = listOf("TAK", "NIE")
                             ),
                             ChoiceField(
-                                fieldNumber = 17,
+                                fieldNumber = 14,
                                 title = "Czy ma Pan(i) podwyższona temperaturę?",
                                 choices = listOf("TAK", "NIE")
                             ),
                             ChoiceField(
-                                fieldNumber = 18,
+                                fieldNumber = 15,
                                 title = "Czy ma Pan(i) kaszel?",
                                 choices = listOf("TAK", "NIE")
                             ),
                             ChoiceField(
-                                fieldNumber = 19,
+                                fieldNumber = 16,
                                 title = "Czy odczuwa Pan(i) duszność, brak tchu?",
                                 choices = listOf("TAK", "NIE")
                             )
                         ),
                         derived = listOf(
                             DerivedField(
-                                fieldNumber = 3,
+                                fieldNumber = 2,
                                 derivedType = DerivedType.BIRTHDAY_PESEL,
-                                titles = listOf("PESEL", "Data urodzin"),
-                                descriptions = listOf("PESEL pacjenta: ", "Data urodzin pacjenta:")
+                                titles = listOf("PESEL", "Data urodzenia")
                             )
                         ),
                         slider = listOf(
                             SliderField(
-                                fieldNumber = 5,
+                                fieldNumber = 3,
                                 fieldType = FieldType.HIDDEN,
                                 inline = false,
                                 title = "Temperatura",
@@ -124,7 +105,7 @@ class DatabaseInitializer {
                                 step = 0.1
                             ),
                             SliderField(
-                                fieldNumber = 6,
+                                fieldNumber = 4,
                                 fieldType = FieldType.HIDDEN,
                                 inline = false,
                                 title = "Ciśnienie skurczowe",
@@ -134,7 +115,7 @@ class DatabaseInitializer {
                                 step = 10.0
                             ),
                             SliderField(
-                                fieldNumber = 7,
+                                fieldNumber = 5,
                                 fieldType = FieldType.HIDDEN,
                                 inline = false,
                                 title = "Tętno",
@@ -144,27 +125,27 @@ class DatabaseInitializer {
                                 step = 10.0
                             ),
                             SliderField(
-                                fieldNumber = 9,
+                                fieldNumber = 6,
                                 fieldType = FieldType.HIDDEN,
                                 inline = false,
                                 title = "Saturacja",
                                 minValue = 80.0,
                                 maxValue = 100.0,
                                 defaultValue = 95.0,
-                                step = 5.0
+                                step = 1.0
                             )
                         ),
                         text = listOf(
                             TextField(
-                                fieldNumber = 1,
+                                fieldNumber = 0,
                                 title = "Nazwisko"
                             ),
                             TextField(
-                                fieldNumber = 2,
-                                title = "Imiona"
+                                fieldNumber = 1,
+                                title = "Imię"
                             ),
                             TextField(
-                                fieldNumber = 20,
+                                fieldNumber = 17,
                                 fieldType = FieldType.HIDDEN,
                                 inline = false,
                                 title = "Dodatkowe objawy",
