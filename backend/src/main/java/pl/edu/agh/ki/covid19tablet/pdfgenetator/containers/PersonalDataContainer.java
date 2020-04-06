2c5118d0b8ed7757f08c857e5da4201747daeabc
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-04-06 19:00:11 +0200

    Added PESEL handling

--- backend/src/main/java/pl/edu/agh/ki/covid19tablet/pdfgenetator/containers/PersonalDataContainer.java
+++ backend/src/main/java/pl/edu/agh/ki/covid19tablet/pdfgenetator/containers/PersonalDataContainer.java
@@ -1,7 +1,11 @@
 package pl.edu.agh.ki.covid19tablet.pdfgenetator.containers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.Nullable;
 import pl.edu.agh.ki.covid19tablet.form.Form;
 import pl.edu.agh.ki.covid19tablet.schema.fields.DerivedField;
+import pl.edu.agh.ki.covid19tablet.schema.fields.DerivedType;
 import pl.edu.agh.ki.covid19tablet.schema.fields.TextField;
 import pl.edu.agh.ki.covid19tablet.state.fields.DerivedFieldState;
 import pl.edu.agh.ki.covid19tablet.state.fields.TextFieldState;
@@ -10,8 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PersonalDataContainer {
-
     private List<PersonalData> personalDataList;
+
 
     public PersonalDataContainer(Form form) {
         this.personalDataList = extractPersonalData(form);
@@ -71,10 +75,19 @@ public class PersonalDataContainer {
         for (int i = 0; i < derivedFields.size() && i < derivedFieldStates.size(); i++) {
             List<String> titles = derivedFields.get(i).getTitles();
             List<String> values = derivedFieldStates.get(i).getValue();
+            DerivedField derivedField = derivedFields.get(i);
             for (int j = 0; j < titles.size() && j < values.size(); j++) {
-                String title = titles.get(j);
                 String value = values.get(j);
+                if (value.isEmpty()) continue;
 
+                if (j == 0 && derivedField.getDerivedType() == DerivedType.BIRTHDAY_PESEL) {
+                    DerivedPolishTypeData data = extractPersonal(value);
+                    if (data == null) continue;
+                    extractedPersonalData.add(new PersonalData(data.getType() + ':', data.getValue()));
+                    continue;
+                }
+
+                String title = titles.get(j);
                 if (title.charAt(title.length() - 1) != ':') {
                     title = title + ':';
                 }
@@ -84,5 +97,16 @@ public class PersonalDataContainer {
         }
 
         return extractedPersonalData;
+    }
+
+    @Nullable
+    private DerivedPolishTypeData extractPersonal(String personalJSON) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(personalJSON, DerivedPolishTypeData.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
