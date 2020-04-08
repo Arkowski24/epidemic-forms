abdbcad3daa488320cf360669f12bc2371467a73
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-04-08 06:52:41 +0200

    Reverted form deletion

--- backend/src/main/kotlin/pl/edu/agh/ki/covid19tablet/user/employee/EmployeeController.kt
+++ backend/src/main/kotlin/pl/edu/agh/ki/covid19tablet/user/employee/EmployeeController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -44,7 +45,7 @@ class EmployeeController(
     fun createEmployee(@Valid @RequestBody request: CreateEmployeeRequest): EmployeeDTO =
         employeeService.createEmployee(request)
 
-    @PostMapping
+    @PutMapping
     @PreAuthorize("hasAuthority('${Authorities.EMPLOYEE_MODIFY}')")
     fun modifyEmployee(
         @PathVariable employeeId: EmployeeId,
